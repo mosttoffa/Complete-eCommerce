d3e17f9ad44c899534c0d636375b48ca42e02349
mosttoffa <mostofacba@gmail.com>
2021-02-17 05:10:06 +0600

    Create BillingModels.

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+# abc@gmail.com -->> 1000000 billing profiles  but
+# user abc@gmail.com -- 1 billing profile 
+
+class BillingProfile(models.Model):
+    user        = models.OneToOneField(User, null=True, blank=True, on_delete=models.SET_NULL)   #  .CASCADE
+    email       = models.EmailField()
+    active      = models.BooleanField(default=True)
+    update      = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    # customer_id in payment method/bank.....
+
+
+    def __str__(self):
+        return self.email
+
+
+# def billing_profile_created_receiver(sender, instance, created, *args, **kwargs):
+#     if created:
+#         print("Actual API Request Send to bankkkkk....")
+#         instance.customer_id = newID
+#         instance.save()
+
+
+def user_created_receiver(sender, instance, created, *args, **kwargs):
+    if created and instance.email:
+        BillingProfile.objects.get_or_create(user=instance, email=instance.email)
+   
+post_save.connect(user_created_receiver, sender=User)
+
